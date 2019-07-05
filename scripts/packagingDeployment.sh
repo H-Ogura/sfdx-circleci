909b405a30458d880461757465f50b1dcc63e48e
ogurah <ogurah@fsi.co.jp>
2019-07-05 15:34:11 +0900

    検証環境に合わせて修正

--- scripts/packagingDeployment.sh
+++ scripts/packagingDeployment.sh
@@ -2,18 +2,21 @@
 
 # Default values
 BRANCH=$1
-SFDX_CLI_EXEC=sfdx
+SFDX_CLI_EXEC="sfdx"
 TARGET_ORG='packaging@trailheadapps.org'
+TARGETDEVHUBUSERNAME="ogurah@curious-panda-m1t97k.com"
 RESULT=0
 
 # Defining Salesforce CLI exec, depending if it's CI or local dev machine
+# CircleCiで実行する場合、$CIは常にtrue.
+# ローカルで実行した場合はここを通らない。
 if [ $CI ]; then
   echo "Script is running on CI"
-  SFDX_CLI_EXEC=node_modules/sfdx-cli/bin/run
-  TARGET_ORG="packagingorg"
+  SFDX_CLI_EXEC="sfdx"
+  TARGET_ORG="DXVerification"
 fi
 
-PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:create -p dreamhouse -x -w 10 --json)"
+PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:create -p DXVerificationCi -x -w 10 --json)"
 RESULT="$(echo $PACKAGE_VERSION | jq '.status')"
 echo "Result is $RESULT"
 
@@ -30,5 +33,9 @@ fi
 
 PACKAGE_VERSION="$(echo $PACKAGE_VERSION | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 
+# 参考のために残す
+# $SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $TARGET_ORG -r
+$SFDX_CLI_EXEC force:package:version:promote -p $PACKAGE_VERSION -n -v $TARGETDEVHUBUSERNAME
 $SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $TARGET_ORG -r
+
 echo "Done"
